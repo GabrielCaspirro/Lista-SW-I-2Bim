1c38d4604bc2352faa9ef12c68d7b04600e30801
Gabriel Caspirro Demarchi <124791810+GabrielCaspirro@users.noreply.github.com>
2024-05-17 17:52:13 -0300

    Add files via upload

---
+++ ex09/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Exemplo de uso
+        try
+        {
+            Livro livro = new Livro("", "Autor Teste");
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        try
+        {
+            Livro livro2 = new Livro("Título Teste", ""); 
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        Livro livro3 = new Livro("Harry Potter", "J.K. Rowling");
+        Console.WriteLine($"Título: {livro3.Titulo}, Autor: {livro3.Autor}");
+    }
+}
